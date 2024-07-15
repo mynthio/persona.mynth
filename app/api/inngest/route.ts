31fb9b71adbda473418f54a843be201fa6abfefa
Tomasz Szczuciński <tomasz.szczucinski@mynth.io>
2024-07-16 00:38:52 +0200

    change max duration for inngest functions

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,6 +2,8 @@ import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import * as functions from "@/inngest/functions";
 
+export const maxDuration = 300;
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [...Object.values(functions)],
